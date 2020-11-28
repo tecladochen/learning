8bbf3fd6c7a5fe848f88368c8a259b3e0d553bc2
tecladochen <1491949774@qq.com>
2020-11-28 15:47:03 +0800

    submit Person

--- Java/Experiments/src/com/Tecladochen/Person.java
+++ Java/Experiments/src/com/Tecladochen/Person.java
@@ -1,6 +1,8 @@
 package com.Tecladochen;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class Person {
     private String name,address,number,email;
@@ -10,11 +12,25 @@ public class Person {
         this.number = number;
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", number='" + number + '\'' +
+                ", email='" + email + '\'';
+    }
 }
 class Student extends Person {
+    private final String rank;
     public Student(String name, String address, String number, String email, String rank){
         super(name,address,number,email);
-        final  String ranks = rank;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Grade ='" + rank + '\'';
     }
 }
 class Employee extends Person {
@@ -27,6 +43,14 @@ class Employee extends Person {
         this.office = office;
         this.salary = salary;
         employtime = new MyDate(year, month, day);
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "office='" + office + '\'' +
+                ", salary=" + salary +
+                ", employtime=" + employtime.toString() ;
     }
 }
 class Faculty extends Employee {
@@ -41,6 +65,14 @@ class Faculty extends Employee {
         this.workend = workend;
         this.rank = rank;
     }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "workstart=" + workstart +
+                ", workend=" + workend +
+                ", rank='" + rank + '\'';
+    }
 }
 class Staff extends Employee {
     private String call;
@@ -48,6 +80,12 @@ class Staff extends Employee {
                  int salary,int year, int month, int day,String call){
         super(name,address,number,email,office,salary,year,month,day);
         this.call = call;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "call='" + call + '\'';
     }
 }
 class MyDate {
@@ -92,5 +130,61 @@ class MyDate {
         year = now.get(GregorianCalendar.YEAR);
         month = now.get(GregorianCalendar.MONTH);
         day = now.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public String toString() {
+        return "Employment time is " +
+                year + "年" +
+                month + "月" +
+                day + "日 ";
+    }
+}
+class TestPerson{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter necessary name,address,Tel and email :");
+        String name = input.next();
+        String address = input.next();
+        String numbers = input.next();
+        String email = input.next();
+        System.out.print("Please select (Student/Employee) :");
+        String select1 = input.next();
+        if(select1.equals("Student")){
+
+            System.out.print("Please select student's grade :");
+            String grade = input.next();
+            Student student1 = new Student(name,address,numbers,email,grade);
+            System.out.print(student1.toString());
+        }
+        else{
+            System.out.print("Please enter necessary office and salary :");
+            String office = input.next();
+            int salary = input.nextInt();
+            System.out.print("Please enter employment time (Example:2020 2 23) :");
+            int year = input.nextInt();
+            int month = input.nextInt();
+            int day = input.nextInt();
+            System.out.print("Please select (Faculty/Staff) :");
+            String select2 = input.next();
+            if(select2.equals("Faculty")){
+                System.out.println("Please enter necessary Worktime and rank :");
+                System.out.print("WorkStart time is ");
+                double workstart = input.nextDouble();
+                System.out.print("WorkEnd time is ");
+                double workend = input.nextDouble();
+                System.out.print("The rank is ");
+                String rank = input.next();
+                Faculty faculty1 = new Faculty(name,address,numbers,email,office,salary,year,month,
+                        day,workstart,workend,rank);
+                System.out.print(faculty1.toString());
+            }
+            else{
+                System.out.print("Please enter necessary Position :");
+                String call = input.next();
+                Staff staff1 = new Staff(name,address,numbers,email,office,salary,year,month,day,call);
+                System.out.print(staff1.toString());
+            }
+        }
     }
 }
